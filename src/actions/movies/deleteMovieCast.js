6c691990bda6aa3f735aec16b14a2aa56f3ad7d9
vocafeuvre <hirafura96@gmail.com>
2019-05-29 00:36:07 +0800

    Modified delete movie cast endpoint, should not return any data

--- src/actions/movies/deleteMovieCast.js
+++ src/actions/movies/deleteMovieCast.js
@@ -13,12 +13,8 @@ module.exports = function(req, res) {
       } else {
         if (movie) {
           movie.cast.id(req.params.castId).remove();
-          var savedMovie = await movie.save();
-          res.send({
-            data: {
-              movie: movieDetails(savedMovie)
-            }
-          });
+          await movie.save();
+          res.status(204).send(`Cast ${req.params.castId} has been removed`);
         } else {
           res.status(404).send(Constant.NOT_FOUND_REQUEST_MESSAGE);
         }
