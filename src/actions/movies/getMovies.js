236094456d230a85a3303e61ebd63ad2d7009875
vocafeuvre <hirafura96@gmail.com>
2019-05-28 21:55:35 +0800

    Added regex insensitive conditions to title, and hasAll condition to tags

--- src/actions/movies/getMovies.js
+++ src/actions/movies/getMovies.js
@@ -3,14 +3,23 @@ const Constant = require("../../constants");
 const movieStub = require("../../responders/movieStub");
 
 module.exports = function(req, res) {
-    const query = {};
+  const query = {};
+  var page = 1;
 
   if (req.query.title) {
-    query.title = req.query.title;
+    query.title = new RegExp(req.query.title, "i");
   }
 
   if (req.query.tags) {
-    query.tags = req.query.tags;
+    query.tags = {
+      $all: req.query.tags.split(",").map(function(val) {
+        return val.trim();
+      })
+    };
+  }
+
+  if (req.query.page) {
+    page = Number.parseInt(req.query.page);
   }
 
   Movie.find(query, function(err, movies) {
@@ -18,18 +27,22 @@ module.exports = function(req, res) {
       console.log(err);
       res.status(500).send(Constant.ERR_REQUEST_MESSAGE);
     } else {
+      const pageStart = (page - 1) * Constant.PER_PAGE;
+      const pageEnd = page * Constant.PER_PAGE;
       res.send({
         data: {
           movies: {
-            items: movies.slice(0, Constant.PER_PAGE).map(function(val) {
-              return movieStub(val);
-            }),
+            items: movies
+              .slice(pageStart, pageEnd)
+              .map(function(val) {
+                return movieStub(val);
+              }),
             perPage: Constant.PER_PAGE,
-            currentPage: 1,
+            currentPage: page,
             total: movies.length
           }
         }
       });
     }
   });
-}
+};
