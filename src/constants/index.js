9b3a2ebbc81a8c1022eaf8cf46b14b4011dd3cdb
vocafeuvre <hirafura96@gmail.com>
2019-05-28 21:18:32 +0800

    Added constants for the app

---
+++ src/constants/index.js
@@ -0,0 +1,18 @@
+const PER_PAGE = 10
+const TAGS_PER_STUB = 3
+const CAST_PER_STUB = 3
+const CHARS_IN_TRUNCATE_SUMMARY = 120
+
+const ERR_REQUEST_MESSAGE = "An error has occurred while making this request."
+const BAD_PARAMS_REQUEST_MESSAGE = "This request has wrong parameters."
+const NOT_FOUND_REQUEST_MESSAGE = "The requested entity was not found."
+
+module.exports = {
+    PER_PAGE,
+    TAGS_PER_STUB,
+    CHARS_IN_TRUNCATE_SUMMARY,
+    CAST_PER_STUB,
+    ERR_REQUEST_MESSAGE,
+    BAD_PARAMS_REQUEST_MESSAGE,
+    NOT_FOUND_REQUEST_MESSAGE
+}
