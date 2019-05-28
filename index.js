7dfc5dbb5be2609789d3532209fd9004af5e10f9
vocafeuvre <hirafura96@gmail.com>
2019-05-28 21:17:26 +0800

    Fixed app requiring in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { app } = require("./src/server.js")
+const app = require("./src/server.js")
 const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => console.log(`APP is being served at port ${PORT}!`))
