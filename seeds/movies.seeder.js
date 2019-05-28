3bea879870475b152fa458c11862addf5c4d0654
vocafeuvre <hirafura96@gmail.com>
2019-05-28 21:15:55 +0800

    Updated seeding, will drop collection everytime the user decides to seed again

--- seeds/movies.seeder.js
+++ seeds/movies.seeder.js
@@ -1,14 +1,13 @@
 var Seeder = require("mongoose-data-seed").Seeder;
 var Movie = require("../src/models/movies");
-var data = require("./data")
+var data = require("./data");
 
 var MoviesSeeder = Seeder.extend({
   shouldRun: async function() {
-    return await Movie.countDocuments()
-      .exec()
-      .then(count => { return count === 0 });
+    return true;
   },
   run: async function() {
+    await Movie.deleteMany({});
     return await Movie.create(data);
   }
 });
