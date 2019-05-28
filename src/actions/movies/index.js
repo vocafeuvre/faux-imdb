157ab094c40033a342038b11d1896a5d8a35a2f1
vocafeuvre <hirafura96@gmail.com>
2019-05-28 21:54:29 +0800

    Removed duplicate resource endpoint

--- src/actions/movies/index.js
+++ src/actions/movies/index.js
@@ -6,10 +6,10 @@ const addMovieCast = require("./addMovieCast")
 const updateMovieCast = require("./updateMovieCast")
 const deleteMovieCast = require("./deleteMovieCast")
 
-router.get("/movies", getMovies);
-router.get("/movies/:id/details", getMovieDetails);
-router.post("/movies/:id/cast", addMovieCast);
-router.put("/movies/:id/cast/:castId", updateMovieCast);
-router.delete("/movies/:id/cast/:castId", deleteMovieCast);
+router.get("/", getMovies);
+router.get("/:id/details", getMovieDetails);
+router.post("/:id/cast", addMovieCast);
+router.put("/:id/cast/:castId", updateMovieCast);
+router.delete("/:id/cast/:castId", deleteMovieCast);
 
 module.exports = router
